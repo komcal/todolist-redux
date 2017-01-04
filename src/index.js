5bc4905a2dd3aa5bca842237f16d2dfc307f0f98
komcal <kom.sujautra@gmail.com>
2017-01-04 15:45:06 +0700

    add file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 import App from './App';
 import './index.css';
 
+let store = createStore(todoApp)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
