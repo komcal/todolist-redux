0fca305e068d2766764259a1e2a904aa8805f176
komcal <kom.sujautra@gmail.com>
2017-02-05 17:45:11 +0700

    add actions test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -11,4 +11,28 @@ describe('Actions', () => {
     }
     expect(actions.addTodo(text)).toEqual(expectedAction)
   })
+
+  it('should create and action to toggle todo', () => {
+    const expectedAction = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    }
+    expect(actions.toggleTodo(1)).toEqual(expectedAction);
+  })
+
+  it('should create and action to remove todo', () => {
+    const expectedAction = {
+      type: 'REMOVE_TODO',
+      id: 1
+    }
+    expect(actions.removeTodo(1)).toEqual(expectedAction);
+  })
+
+  it('should create and action to setVisibilityFilter', () => {
+    const expectedAction = {
+      type: 'SET_VISIBILITY_FILTER',
+      filter: 'SHOW_COMPLETED'
+    }
+    expect(actions.setVisibilityFilter('SHOW_COMPLETED')).toEqual(expectedAction);
+  })
 })
